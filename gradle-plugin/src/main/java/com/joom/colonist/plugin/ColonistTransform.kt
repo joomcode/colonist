2c8fcc7636857cc48fef3c710ac59af3419267a1
Michael Rozumyanskiy <mr@joom.com>
2020-10-01 13:26:51 +0300

    Add PROVIDED_ONLY scope to referenced scopes to make the plugin work with GAP 4.1.0

--- gradle-plugin/src/main/java/com/joom/colonist/plugin/ColonistTransform.kt
+++ gradle-plugin/src/main/java/com/joom/colonist/plugin/ColonistTransform.kt
@@ -85,7 +85,8 @@ class ColonistTransform(private val project: Project) : Transform() {
   override fun getReferencedScopes(): MutableSet<in QualifiedContent.Scope> {
     return EnumSet.of(
       QualifiedContent.Scope.EXTERNAL_LIBRARIES,
-      QualifiedContent.Scope.TESTED_CODE
+      QualifiedContent.Scope.TESTED_CODE,
+      QualifiedContent.Scope.PROVIDED_ONLY,
     )
   }
 
