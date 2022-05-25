ac6592928909bd37990f80374f17ca04ec676ef7
Dmitry Kostyrev <dkostyrev@joom.com>
2022-05-25 16:56:47 +0300

    Introduce more cases in ModularColony.

--- colonist-samples/modular-java/modular-colony/src/main/java/com/joom/colonist/modular/colony/ModularColony.java
+++ colonist-samples/modular-java/modular-colony/src/main/java/com/joom/colonist/modular/colony/ModularColony.java
@@ -2,8 +2,12 @@ package com.joom.colonist.modular.colony;
 
 import com.joom.colonist.Colonist;
 import com.joom.colonist.OnAcceptSettler;
+import com.joom.colonist.OnProduceSettler;
 
 @ModularColonyAnnotation
+@ModularColonyProduceAnnotation
+@ModularColonyProduceAndForgetAnnotation
+@ModularColonyProduceStaticAnnotation
 public class ModularColony {
 
   public void settle() {
@@ -12,7 +16,50 @@ public class ModularColony {
   }
 
   @OnAcceptSettler(colonyAnnotation = ModularColonyAnnotation.class)
-  public void onAcceptSettler(Class<?> settler) {
-    System.out.println("Accepted " + settler.getName());
+  private void onAcceptSettler(Object settler) {
+    System.out.println("Accepted " + settler.getClass().getName());
+  }
+
+  @OnProduceSettler(colonyAnnotation = ModularColonyProduceAnnotation.class)
+  private Object onProduceSettler(Class<?> settler) {
+    System.out.println("Producing " + settler.getName());
+
+    try {
+      return settler.newInstance();
+    } catch (Throwable throwable) {
+      throw new RuntimeException("Failed to create settler " + settler.getName(), throwable);
+    }
+  }
+
+  @OnProduceSettler(colonyAnnotation = ModularColonyProduceAndForgetAnnotation.class)
+  private Object onProduceAndForgetSettler(Class<?> settler) {
+    System.out.println("Producing and forgetting " + settler.getName());
+
+    try {
+      return settler.newInstance();
+    } catch (Throwable throwable) {
+      throw new RuntimeException("Failed to create settler " + settler.getName(), throwable);
+    }
+  }
+
+  @OnAcceptSettler(colonyAnnotation = ModularColonyProduceAnnotation.class)
+  private void onAcceptProducedSettler(Object settler) {
+    System.out.println("Accepted produced settler " + settler.getClass().getName());
+  }
+
+  @OnProduceSettler(colonyAnnotation = ModularColonyProduceStaticAnnotation.class)
+  private static Object onProduceSettlerStatic(Class<?> settler) {
+    System.out.println("Producing via static " + settler.getName());
+
+    try {
+      return settler.newInstance();
+    } catch (Throwable throwable) {
+      throw new RuntimeException("Failed to create settler " + settler.getName(), throwable);
+    }
+  }
+
+  @OnAcceptSettler(colonyAnnotation = ModularColonyProduceStaticAnnotation.class)
+  private static void onAcceptProducedSettlerStatic(Object settler) {
+    System.out.println("Accepted produced settler via static " + settler.getClass().getName());
   }
 }
--- colonist-samples/modular-java/modular-colony/src/main/java/com/joom/colonist/modular/colony/ModularColonyAnnotation.java
+++ colonist-samples/modular-java/modular-colony/src/main/java/com/joom/colonist/modular/colony/ModularColonyAnnotation.java
@@ -1,14 +1,50 @@
 package com.joom.colonist.modular.colony;
 
+import com.joom.colonist.AcceptSettlersAndForget;
 import com.joom.colonist.AcceptSettlersViaCallback;
 import com.joom.colonist.Colony;
 import com.joom.colonist.ProduceSettlersAsClasses;
+import com.joom.colonist.ProduceSettlersViaCallback;
+import com.joom.colonist.ProduceSettlersViaConstructor;
 import com.joom.colonist.SelectSettlersByAnnotation;
 import com.joom.colonist.modular.api.Settler;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Colony
 @SelectSettlersByAnnotation(Settler.class)
 @AcceptSettlersViaCallback
-@ProduceSettlersAsClasses
+@ProduceSettlersViaConstructor
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.CLASS)
 @interface ModularColonyAnnotation {
+}
+
+@Colony
+@SelectSettlersByAnnotation(Settler.class)
+@AcceptSettlersViaCallback
+@ProduceSettlersViaCallback
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.CLASS)
+@interface ModularColonyProduceAnnotation {
+}
+
+@Colony
+@SelectSettlersByAnnotation(Settler.class)
+@AcceptSettlersViaCallback
+@ProduceSettlersViaCallback
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.CLASS)
+@interface ModularColonyProduceStaticAnnotation {
+}
+
+@Colony
+@SelectSettlersByAnnotation(Settler.class)
+@AcceptSettlersAndForget
+@ProduceSettlersViaCallback
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.CLASS)
+@interface ModularColonyProduceAndForgetAnnotation {
 }
